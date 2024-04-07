7e373d5033f1cdea40737e74a46aebcc677f8684
veudal <62218506+veudal@users.noreply.github.com>
2024-04-07 15:55:20 +0200

    Pads have acceleration and ball bounces away from pads

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const multiplicator = 1.05
+const lineWidth = 1;
+const maxVelocity = 2;
 let firstPlayerPos, secondPlayerPos;
+let firstPlayerVelocity = secondPlayerVelocity = 0
 let padHeight;
 let padWidth;
 let padMargin;
+let toggle = false
 var keysPressed = {};
 let ball = {
   x: 0,
@@ -19,36 +23,78 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const canvas = document.getElementById("canvas")
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
-    padHeight = canvas.height / 8;
+    padHeight = canvas.height / 8; //To 8
     padMargin = padHeight / 3;
     padWidth = padHeight / 5;
     firstPlayerPos = secondPlayerPos = (canvas.height / 2) - (padHeight / 2);
 
-    ball.x = canvas.width / 2 - ball.radius
+    ball.x = canvas.width / 2
     ball.y = canvas.height / 2
 
     if(Math.random() > 0.5){
       ball.velocity.x = -ball.velocity.x
+      toggle = true
     }
     if(Math.random() > 0.5){
       ball.velocity.y = -ball.velocity.y
     }
 
 
-    var context = canvas.getContext('2d');
-    context.fillStyle = "white"
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "white"
 
-    setInterval(function(){
-      update(context), 16
-    });
+    drawGameState(ctx);
+    setTimeout(function() {
+      setInterval(function(){
+        update(ctx), 16
+      });
+    }, 250); //To 1000
 })
 
+
 document.addEventListener('keydown', function(event) {
-  keysPressed[event.key] = true;
+  let key = event.key.toUpperCase();
+  keysPressed[key] = true;
+
+  let direction
+  if(key == "ARROWUP" || key == "W"){
+    direction = "up"  
+  }
+  else{
+    direction = "down"
+  }
+  if(key == "W" || key == "S"){
+    firstPlayerVelocity = handlePlayerMovement(firstPlayerVelocity, direction);
+  }
+  else{
+    secondPlayerVelocity = handlePlayerMovement(secondPlayerVelocity, direction);
+  }
+
 });
 
+
+function handlePlayerMovement(playerVelocity, direction) {
+  if (direction == "up") {
+      if (playerVelocity > 0) {
+          playerVelocity = 0;
+      }
+      if (playerVelocity > -maxVelocity) {
+          playerVelocity -= 1;
+      }
+  } else if (direction == "down") {
+      if (playerVelocity < 0) {
+          playerVelocity = 0;
+      }
+      if (playerVelocity < maxVelocity) {
+          playerVelocity += 1;
+      }
+  }
+  return playerVelocity
+}
+
 document.addEventListener('keyup', function(event) {
-  delete keysPressed[event.key];
+  let key = event.key.toUpperCase();
+  delete keysPressed[key];
 });
 
    
@@ -66,13 +112,23 @@ function checkPos(pos) {
 
 function update(ctx){
 
+    firstPlayerVelocity += Math.sign(firstPlayerVelocity) * (-0.05)
+    secondPlayerVelocity += Math.sign(secondPlayerVelocity) * (-0.05)
+
+    console.log(firstPlayerVelocity)
+    console.log(secondPlayerVelocity)
+
     updatePads()
     updateBall()
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(padMargin, firstPlayerPos, padWidth, padHeight)
-    ctx.fillRect(canvas.width - padMargin - padWidth, secondPlayerPos, padWidth, padHeight)
-    drawCircle(ctx, ball.x, ball.y, ball.radius, "white")
+    drawGameState(ctx);
+}
 
+function drawGameState(ctx) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCircle(ctx, ball.x, ball.y, ball.radius);
+  ctx.fillRect(canvas.width / 2 - lineWidth, 0, lineWidth, canvas.height);
+  ctx.fillRect(padMargin, firstPlayerPos, padWidth, padHeight);
+  ctx.fillRect(canvas.width - padMargin - padWidth, secondPlayerPos, padWidth, padHeight);
 }
 
 function updateBall(){
@@ -80,12 +136,27 @@ function updateBall(){
 
   var maxY = canvas.height - ball.radius;
   if(ball.y > maxY || ball.y < ball.radius){
-    ball.velocity.y = -ball.velocity.y * multiplicator;
+    ball.velocity.y = -ball.velocity.y;
   }
+
+  //Left pad
+  if(toggle == true && ball.x > padMargin + padWidth && ball.x < ball.radius + padWidth + padMargin && ball.y > firstPlayerPos - ball.radius && ball.y < firstPlayerPos + padHeight + ball.radius){
+    ball.velocity.x = -ball.velocity.x * multiplicator;
+    ball.velocity.y = -ball.velocity.y * multiplicator;
+    toggle = !toggle;
+  }
+
+  //Right pad
+  if(toggle == false && ball.x < canvas.width - padMargin - padWidth && ball.x > canvas.width - padWidth - padMargin - ball.radius && ball.y > secondPlayerPos - ball.radius && ball.y < secondPlayerPos + padHeight + ball.radius){
+    ball.velocity.x = -ball.velocity.x * multiplicator;
+    ball.velocity.y = -ball.velocity.y * multiplicator;
+    toggle = !toggle;
+  }
+
 
   var maxX = canvas.width - ball.radius;
   if(ball.x > maxX || ball.x < ball.radius){
-    ball.velocity.x = -ball.velocity.x * multiplicator;
+    ball.velocity.x = -ball.velocity.x;
     //GOAL
   }
 
@@ -97,27 +168,24 @@ function updateBall(){
 
 function updatePads(){
 
-  if (keysPressed["w"]) {
-    firstPlayerPos = firstPlayerPos - 2;
+  if (keysPressed["W"]) {
+    firstPlayerPos = firstPlayerPos + firstPlayerVelocity - 2;
   }
-  if (keysPressed["s"]) {
-    firstPlayerPos = firstPlayerPos + 2;
+  if (keysPressed["S"]) {
+    firstPlayerPos = firstPlayerPos + firstPlayerVelocity + 2;
   }
-  if (keysPressed["ArrowUp"]) {
-    secondPlayerPos = secondPlayerPos - 2;
+  if (keysPressed["ARROWUP"]) {
+    secondPlayerPos = secondPlayerPos + secondPlayerVelocity - 2;
   }
-  if (keysPressed["ArrowDown"]) {
-    secondPlayerPos = secondPlayerPos + 2;
+  if (keysPressed["ARROWDOWN"]) {
+    secondPlayerPos = secondPlayerPos + secondPlayerVelocity + 2;
   }
   firstPlayerPos = checkPos(firstPlayerPos)
   secondPlayerPos = checkPos(secondPlayerPos)
 }
 
-function drawCircle(ctx, x, y, radius, fill) {
+function drawCircle(ctx, x, y, radius) {
     ctx.beginPath()
     ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
-    if (fill) {
-      ctx.fillStyle = fill
-      ctx.fill()
-    }
+    ctx.fill()
   }
