46528213d3084bb3cec6d45a01b9d7d889a42ad8
veudal <62218506+veudal@users.noreply.github.com>
2024-04-07 17:44:17 +0200

    Added sfx, favicon, custom font and dashed line

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const multiplicator = 1.05
-const lineWidth = 1;
-const maxVelocity = 2;
+const lineWidth = 3;
 let scoreMargin = 128
 let firstPlayerScore = 0
 let secondPlayerScore = 0
@@ -13,7 +12,7 @@ var keysPressed = {};
 let ball = {
   x: 0,
   y: 0,
-  radius: 1,
+  radius: 0,
   velocity: {
     x: 0,
     y: 0
@@ -104,13 +103,23 @@ function drawGameState(ctx) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawCircle(ctx, ball.x, ball.y, ball.radius);
   drawScore(ctx)
-  ctx.fillRect(canvas.width / 2 - lineWidth, 0, lineWidth, canvas.height);
+  drawDashLine(ctx);
   ctx.fillRect(padMargin, firstPlayerPos, padWidth, padHeight);
   ctx.fillRect(canvas.width - padMargin - padWidth, secondPlayerPos, padWidth, padHeight);
 }
 
+function drawDashLine(ctx) {
+  ctx.setLineDash([8, 8]);
+  ctx.beginPath();
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = "white";
+  ctx.moveTo(canvas.width / 2 - lineWidth, 0);
+  ctx.lineTo(canvas.width / 2 - lineWidth, canvas.height);
+  ctx.stroke();
+}
+
 function drawScore(ctx) {
-  ctx.font = "64px 'copperplate', Fantasy";
+  ctx.font = "48px 'Pixel', Fantasy";
 
   const canvasCenterX = canvas.width / 2;
   const textMetrics1 = ctx.measureText(firstPlayerScore);
@@ -128,11 +137,13 @@ function updateBall(){
   //Bottom and top
   var maxY = canvas.height - ball.radius;
   if(ball.y > maxY || ball.y < ball.radius){
+    playSound("collision")
     ball.velocity.y = -ball.velocity.y;
   }
 
   //Left pad
   if(toggle == true && ball.x > padMargin + padWidth - ball.radius && ball.x < ball.radius + padWidth + padMargin && ball.y > firstPlayerPos - ball.radius && ball.y < firstPlayerPos + padHeight + ball.radius){
+    playSound("hit")
     ball.velocity.x = -ball.velocity.x * multiplicator;
     let difference = firstPlayerPos + padHeight / 2 - ball.y
     ball.velocity.y = -ball.velocity.y - difference / 50;
@@ -141,6 +152,7 @@ function updateBall(){
 
   //Right pad
   if(toggle == false && ball.x < canvas.width - padMargin - padWidth + ball.radius && ball.x > canvas.width - padWidth - padMargin - ball.radius && ball.y > secondPlayerPos - ball.radius && ball.y < secondPlayerPos + padHeight + ball.radius){
+    playSound("hit")
     ball.velocity.x = -ball.velocity.x * multiplicator;
     let difference = secondPlayerPos + padHeight / 2 - ball.y
     ball.velocity.y = -ball.velocity.y - difference / 50;
@@ -148,22 +160,21 @@ function updateBall(){
   }
 
   //Goal for first player
-  var maxX = canvas.width + ball.radius * 2;
-  if(ball.x > maxX){
+  if(ball.x > canvas.width + ball.radius * 2){
     firstPlayerScore++
+    playSound("goal")
     startGame()
   }
 
   //Goal for second player
   if(ball.x < -ball.radius * 2) {
     secondPlayerScore++
+    playSound("goal")
     startGame()
   }
 
   ball.x += ball.velocity.x
   ball.y += ball.velocity.y
-
-
 }
 
 function updatePads(){
@@ -190,4 +201,9 @@ function drawCircle(ctx, x, y, radius) {
     ctx.beginPath()
     ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
     ctx.fill()
-  }
+}
+
+function playSound(sound) {
+    var audio = document.getElementById(sound);
+    audio.play();
+}
